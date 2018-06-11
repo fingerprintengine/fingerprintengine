be17186a0c93ac430f9317c04ad7532d6b5a2da1
Jerogee <j.geertzen@cosmion.net>
2018-06-11 18:53:19 +0200

    Adding Classifier to Python client and Updating Docs

--- ClientCode/Python/FingerprintEngineClientTest.py
+++ ClientCode/Python/FingerprintEngineClientTest.py
@@ -2,16 +2,20 @@
 
 import FingerprintEngineClient as efe
 
-client = efe.FingerprintEngineClient('https://fingerprintengine.scivalcontent.com/taco7800/TacoService.svc/', '#########', '********')
+client = efe.FingerprintEngineClient('https://fingerprintengine.scivalcontent.com/taco7800/TacoService.svc/', 'your_username', 'your_password')
 
 fp = client.index('MeSHXml', 'malaria', 'bad disease').toFingerprint()
 for concept in fp:
     print('ConceptID: ' + concept.conceptid + ', rank: ' + concept.rank + ', name: ' + concept.name + ', AFreq: ' + concept.afreq)
 
-
 fp = client.index('SingleFeatureAnalysisXml', 'malaria', 'bad disease').toFeatureFingerprint()
 for concept in fp:
     print('ConceptID: ' + concept.conceptid + ', rank: ' + concept.rank + ', name: ' + concept.name + ', AFreq: ' + concept.afreq)
+
+cr = client.classify('DomainClassifier', 'Clemmensen Reduction converts aldehydes and ketones to corresponding hydrocarbons.').toRanking()
+for category in cr:
+    print('Category: ' + category.name + ', Rank: ' + category.rank)
+
 
 ## print raw FP for XML indexing
 # print(client.index('MeSHXml', 'malaria', 'bad disease'))
